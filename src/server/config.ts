76feaee64d735890b85168fca16bc77be2c643b2
Mahmoud Mousa Hamad <mahmoudmousahamad@gmail.com>
2023-01-02 16:46:21 -0600

    fix config vars

--- src/server/config.ts
+++ src/server/config.ts
@@ -5,15 +5,15 @@ let config: { serverEndpoint: string, PORT: number };
 switch (NODE_ENV) {
     case 'production':
         config = {
-            serverEndpoint: 'http://167.172.133.156/',
-            PORT: 3000,
+            serverEndpoint: 'http://useapplier.com/',
+            PORT: 3001,
         }
         break;
 
     case 'staging':
         config = {
-            serverEndpoint: 'http://167.172.133.156/',
-            PORT: 3000,
+            serverEndpoint: 'http://useapplier.com/',
+            PORT: 3001,
         }
         break;
 
